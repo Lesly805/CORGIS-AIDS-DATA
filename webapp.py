f0284d6a35f26cdc3998fd4e10e681eec5d8674d
Lesly805 <77691413+Lesly805@users.noreply.github.com>
2021-05-24 09:59:14 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,3 +1,9 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json 
+
+app = Flask(__name__)
+
 def render_about():
     return render_template('about.html')
 
