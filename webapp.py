f91acde80933629c4f05d9539351a2046a0604b8
Lesly805 <77691413+Lesly805@users.noreply.github.com>
2021-05-24 17:11:50 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,16 +8,20 @@ app = Flask(__name__)
 def render_about():
     return render_template('about.html')
 
-
 @app.route('/databycountry')
 def render_databycountry():
-    country = request.args['country']
-    return render_template('databycountrydisplay.html', options = get_country_options(), TotalDeaths = fact_by_country(country))
+    return render_template('databycountry.html', options = get_country_options())
     
+@app.route('/data')
+def render_data():
+    country = request.args['country']
+    return render_template('databycountrydisplay.html', options = get_country_options(), countryFact = fact_by_country(country))
+    
+
 def get_country_options():
     listOfCountries = []
     with open('aids.json') as demographics_data:
-        years = jsson.load(demographics_data)
+        years = json.load(demographics_data)
     for year in years:
         if not(year["Country"] in listOfCountries):
             listOfCountries.append(year["Country"])
@@ -26,27 +30,32 @@ def get_country_options():
         options = options + Markup("<option value=\"" + c + "\">" + c + "</option>")
     return options 
 
-def fun_fact_by_country(country):
+
+
+def fact_by_country(country):
     with open('aids.json') as demographics_data:
-        year = json.load(demographics_data)
+        years = json.load(demographics_data)
     highest = 0
     lowest = 15000
     
+    highYear= 1990
+    lowYear= 2015
+       
     for x in years:
         if x["Country"] == country:
-            totaldeaths = x["AIDS-Related Deaths"]["All Ages"] 
+            totaldeaths = x["Data"]["AIDS-Related Deaths"]["All Ages"] 
             if totaldeaths > highest:
                 highest = totaldeaths
                 highYear = x["Year"]
             elif totaldeaths == 0:
     	        lowest = lowest
-            elif totaldeaths < lowest:
+            if totaldeaths < lowest:
                 lowest = totaldeaths
                 lowYear = x["Year"]
         highest = round(highest, 2)
         lowest = round(lowest, 2)
 
-    return "The year with the highest deaths in " + country + " is " + highYear + " (" + str(highest) + "%)" + " and the year with the lowest deaths in " + country + " is " + lowYears + " (" + str(lowest) + "%)"
+    return "The year with the highest deaths in " + country + " is " +  str(highYear) + " (" + str(highest) + ")" + " and the year with the lowest deaths in " + country + " is " + str(lowYear) + " (" + str(lowest) + ")"
 
 
 
